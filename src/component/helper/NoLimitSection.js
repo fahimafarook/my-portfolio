cdd6bddccd48e6f13df372c41d25855a96d85d49
fahima farook <fahimafarook510@gmail.com>
2023-07-29 14:09:52 +0530

    Update NoLimitSection.js

--- src/component/helper/NoLimitSection.js
+++ src/component/helper/NoLimitSection.js
@@ -175,7 +175,7 @@ function NoLimitSection() {
 
 
     return (
-        <div style={{ minHeight: "100vh"}}>
+        <div style={{ Height: "100vh"}}>
         <div className='no-limit-section'>
             <div className='key-board'> 
             <div className='board-row row-1'> 
