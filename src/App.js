43a2253d65307d9b4c879c0ca6bbc491a881a29b
fahima farook <fahimafarook510@gmail.com>
2023-07-30 12:48:48 +0530

    --

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
   useEffect(() => {
     if(showSplashScreen){
       document.body.style.overflow = "hidden";
-      document.getElementById('profilePage').scrollIntoView(); // =0=
+      setTimeout(() => document.getElementById('profilePage').scrollIntoView(), 500)
     }
     else{
       resizeHandler(); // =0=
